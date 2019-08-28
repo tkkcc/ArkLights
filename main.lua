26f2446b8f9e610e2f73aa16c1241aa24f3c5edd
bilabila <bilabila@qq.com>
2019-08-28 15:36:14 +0800

    before 黑曜石

--- main.lua
+++ main.lua
@@ -6,8 +6,9 @@ default_delay = 1
 require("path")
 require("util")
 cron = require("crontab")
-基建 = {"基建点击全部", "换人", "制造站补充", "订单交付",
-          "贸易站加速", "线索接收", "线索布置", "信用奖励"}
+基建 = {"基建点击全部", "基建点击全部", "换人",
+          "制造站补充", "订单交付", "贸易站加速", "线索接收",
+          "线索布置", "信用奖励"}
 
 作战 = {"邮件", "轮次作战"}
 任务 = {"植物种植", "免费强化包", "访问好友基建",
@@ -19,6 +20,8 @@ fight_type_ext = {"PR-A-1", "PR-A-2", "PR-B-1", "PR-B-2", "PR-C-1", "PR-C-2",
                   "PR-D-1", "PR-D-2", "CA-5"}
 insert(fight_type_ext, "CE-5")
 repeat_last(fight_type_ext, 100)
+
+fight_type_ext = {"OF-8", "OF-F3"}
 -- insert(fight_type_ext, "CA-5")
 -- repeat_last(fight_type_ext, 200)
 -- insert(fight_type_ext, "PR-D-2")
@@ -59,6 +62,6 @@ table.shuffle(fight_type)
 -- now(作战,基建,任务,background)
 -- now("信用购买", "干员强化", "公开招募聘用",
 -- "任务")
--- now("基建点击全部")
+-- now(基建)
 cron(map(hc, {{作战, "2,8,14,20"}, {基建, "2,14"}, {任务, 3}, {close, 4},
               {background}, {showALL}}), true)
