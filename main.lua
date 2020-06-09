607e0c187c7757753cc41fadf7f6508664752210
bilabila <bilabila@qq.com>
2020-06-09 21:58:34 +0800

    fix获取网络配置

--- main.lua
+++ main.lua
@@ -26,7 +26,8 @@ repeat_last(fight_type, 500, "CE-5")
 repeat_last(fight_type, 10, "龙门市区")
 table.shuffle(fight_type)
 -- 立即执行
-now("基建副手换人")
+-- now("基建副手换人")
+now(unpack(每日开始))
 -- now(unpack(每八小时))
 -- 半点执行
 cron(map(hc, {{每日开始, 4}, {每八小时, "1,9,17"}, {"后台", "0-23"}}))
