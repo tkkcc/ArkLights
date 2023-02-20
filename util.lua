30021eaa4e4b5a6a4a70adffada5280e2b7a8854
hwj <95569717+227675454@users.noreply.github.com>
2023-02-20 22:11:03 +0800

    fix hd_open_time_end (#111)
    hd_open_time_end

--- util.lua
+++ util.lua
@@ -4886,7 +4886,7 @@ update_state_from_ui = function()
   -- log("fight", fight)
 
   -- 活动开放时间段
-  hd_open_time_end = parse_time("202301210400")
+  hd_open_time_end = parse_time("202302210400")
   hd_shop_open_time_end = parse_time("202302240400")--活动商店关闭时间
   hd2_open_time_end = parse_time("202303210400")
   hd2_shop_open_time_end = parse_time("202303240400")--活动2商店关闭时间
