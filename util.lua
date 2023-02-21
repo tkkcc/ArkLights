c004d81ccd523ee8e5fd73621444c34a1bfaddb1
hwj <227675454@qq.com>
2023-02-21 20:13:21 +0800

    hd2_shop_open_time_end

--- util.lua
+++ util.lua
@@ -4889,7 +4889,7 @@ update_state_from_ui = function()
   hd_open_time_end = parse_time("202303030400")
   hd_shop_open_time_end = parse_time("202302240400")--活动商店关闭时间
   hd2_open_time_end = parse_time("202303210400")
-  hd2_shop_open_time_end = parse_time("202303240400")--活动2商店关闭时间
+  hd2_shop_open_time_end = parse_time("202302240400")--活动2商店关闭时间
   hd_mod = "ss" --活动类型 "故事集"/"ss" 区分活动任务
   hd2_mod = "故事集" 
 
