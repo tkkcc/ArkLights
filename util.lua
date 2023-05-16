531594441ea7d06ee95a199e2fd14a3c04062581
hwj <95569717+227675454@users.noreply.github.com>
2023-05-16 17:47:45 +0800

    孤星活动任务及商店 (#150)
    Signed-off-by: hwj <227675454@qq.com>

--- util.lua
+++ util.lua
@@ -3199,7 +3199,7 @@ show_extra_ui = function()
   ui.addCheckBox(layout, "sand_fire_unstop", "打满也不结束", false)
   newRow(layout)
 
-  addButton(layout, nil, "活动任务与商店（愚人号）",
+  addButton(layout, nil, "活动任务与商店（孤星）",
             make_jump_ui_command(layout, nil,
                                  "extra_mode='活动任务与商店';extra_mode_multi=true;lock:remove(main_ui_lock)"))
 
@@ -4954,7 +4954,7 @@ update_state_from_ui = function()
 
   -- 活动开放时间段
   hd_open_time_end = parse_time("202305220400")
-  hd_shop_open_time_end = parse_time("202305040400") -- 活动商店关闭时间
+  hd_shop_open_time_end = parse_time("202305290400") -- 活动商店关闭时间
   hd2_open_time_end = parse_time("202303210400")
   hd2_shop_open_time_end = parse_time("202302240400") -- 活动2商店关闭时间
   hd_mod = "ss" -- 活动类型 "故事集"/"ss" 区分活动任务和是否活动作战
